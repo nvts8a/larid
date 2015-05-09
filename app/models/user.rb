b66679da556bc9e01836f338af46baffaf286398
Steven Justin <nvts8a@gmail.com>
2015-05-09 10:53:27 -0500

    [#4] Created ApiKey and integration with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :api_keys
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  def session_api_key
+    api_keys.active.session.first_or_create
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509150333) do
+ActiveRecord::Schema.define(version: 20150509152053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_keys", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "access_token"
+    t.string   "scope"
+    t.datetime "expired_at"
+    t.datetime "created_at"
+  end
+
+  add_index "api_keys", ["user_id"], name: "index_api_keys_on_user_id", using: :btree
 
   create_table "fish", force: :cascade do |t|
     t.integer  "fisherman_id"
