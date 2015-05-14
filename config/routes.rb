81a81f3d62f4731e2fedbd6557dea3b248394da3
Steven Justin <nvts8a@gmail.com>
2015-05-13 21:02:47 -0500

    [#16] Removed new and edit from users and act routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   namespace :v1 do
     resources :sessions, only: [:create]
     resources :geolocation, only: [:index]
-    resources :users do
-      resources :activities
+    resources :users, only: [:index, :show, :create, :destroy, :update] do
+      resources :activities, only: [:index, :show, :create, :destroy, :update]
     end
   end
 end
